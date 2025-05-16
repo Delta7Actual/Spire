4772ea3dde9ac3fb0c5a6f298d370f0cf826b59a
Delta7Actual <142044822+Delta7Actual@users.noreply.github.com>
2025-05-16 22:25:45 +0300

    Added more HTML elements

--- include/builder.h
+++ include/builder.h
@@ -16,31 +16,16 @@ typedef struct s_page {
     char *style; // This is optional
 } Page;
 
-// Implement as structs even necessary?
-
-// typedef struct s_heading {
-//     uint8_t mag;
-//     char *content;
-// } Heading;
-
-// typedef struct s_div {
-//     char *content;
-// } Div;
-
-// typedef struct s_anchor {
-//     char *url;
-//     char *content;
-// } Anchor;
-
 Page *page_init(const char *title, const char *style);
 int page_render(Page *page);
 
 int add_heading(Page *page, uint8_t mag, const char *content);
 int add_div(Page *page, const char *content);
 int add_anchor(Page *page, const char *url, const char *content);
+int add_button(Page *page, const char *onclick, const char *content);
+int add_paragraph(Page *page, const char *content);
+int add_image(Page *page, const char *src, const char *alt, uint16_t width, uint16_t height);
 
 // Add more elements
-
-char *read_file(const char *file_path);
 
 #endif
--- src/builder.c
+++ src/builder.c
@@ -1,22 +1,38 @@
 #include "../include/builder.h"
 
+// Util method to append element
+static int append_element(Page *page, const char *element) {
+    size_t new_body_len = strlen(page->body) + strlen(element) + 1;
+
+    page->body = realloc(page->body, new_body_len);
+    if (!page->body) return -1;
+
+    strcat(page->body, element);
+
+    return 0;
+}
+
 Page *page_init(const char *title, const char *style) {
     if (!title) return NULL;
-    log_print(0, "Creating page..");
     
     Page *page = malloc(sizeof(Page));
     if (!page) return NULL;
 
     page->title = strdup(title);
     page->body = strdup("");
-    page->style = style ? strdup(style) : NULL;
+    if (style) { // eg. style -> public/style.css
+        size_t path_len = strlen(style) + strlen("public/.css") + 1;
+        page->style = malloc(path_len);
+        if (!page->style) return NULL;
+        snprintf(page->style, path_len, "public/%s.css", style);
+    } else page-> style = strdup("");
 
+    log_print(1, "Page created successfully!");
     return page;
 }
 
 int page_render(Page *page) {
     if (!page || !page->title) return -1;
-    log_print(0, "Rendering page...");
 
     size_t path_len = strlen(page->title) + strlen("public/.html") + 1;
     page->path = malloc(path_len);
@@ -30,78 +46,90 @@ int page_render(Page *page) {
         fprintf(output,
             "<!DOCTYPE html>\n<html>\n<head>\n<title>%s</title>\n</head><body>\n%s\n</body>\n</html>", 
             page->title, page->body);
-    } else { // <link rel=\"stylesheet\" href=\"public/styleSheet.css\">\n
+    } else {
         fprintf(output,
             "<!DOCTYPE html>\n<html>\n<head>\n<title>%s</title>\n<link rel=\"stylesheet\" href=\"%s\">\n</head><body>\n%s\n</body>\n</html>", 
             page->title, page->style, page->body);
     }
 
     fclose(output);
+
+    log_print(1, "Page rendered successfully!");
     return 0;
 }
 
 int add_heading(Page *page, uint8_t mag, const char *content) {
     if (!page || !content || mag < 1 || mag > 6) return -1;
-    log_print(0, "Building [heading]...");
+    log_print(0, "Building [Heading] ...");
     
-    char heading[1024];
-    snprintf(heading, sizeof(heading), "<h%d>%s</h%d>", mag, content, mag);
+    char heading[1024] = {0};
+    snprintf(heading, sizeof(heading), "<h%d>%s</h%d>\n", mag, content, mag);
 
-    // Append
-    size_t new_body_len = strlen(page->body) + strlen(heading) + 1;
-    page->body = realloc(page->body, new_body_len);
-    if (!page->body) return -1;
-    strcat(page->body, heading);
-
-    return 0;
+    return append_element(page, heading);
 }
 
 int add_div(Page *page, const char *content) {
     if (!page || !content) return -1;
-    log_print(0, "Building [div]...");
+    log_print(0, "Building [Division] ...");
 
-    char div[1024];
-    snprintf(div, sizeof(div), "<div>%s</div>", content);
+    char div[1024] = {0};
+    snprintf(div, sizeof(div), "<div>%s</div>\n", content);
 
-    // Append
-    size_t new_body_len = strlen(page->body) + strlen(div) + 1;
-    page->body = realloc(page->body, new_body_len);
-    if (!page->body) return -1;
-    strcat(page->body, div);
-
-    return 0;
+    return append_element(page, div);
 }
 
 int add_anchor(Page *page, const char *url, const char *content) {
     if (!page || !url || !content) return -1;
-    log_print(0, "Building [anchor]...");
+    log_print(0, "Building [anchor] ...");
 
-    char anchor[1024];
-    snprintf(anchor, sizeof(anchor), "<a href=\"%s\">%s</a>", url, content);
+    char anchor[1024] = {0};
+    snprintf(anchor, sizeof(anchor), "<a href=\"%s\">%s</a>\n", url, content);
 
-    // Append
-    size_t new_body_len = strlen(page->body) + strlen(anchor) + 1;
-    page->body = realloc(page->body, new_body_len);
-    if (!page->body) return -1;
-    strcat(page->body, anchor);
-
-    return 0;
+    return append_element(page, anchor);
 }
 
-char *read_file(const char *file_path) {
-    FILE *file = fopen(file_path, "r");
-    if (!file) return NULL;
+int add_button(Page *page, const char *onclick, const char *content) {
+    if (!page || !content) return -1;
+    log_print(0, "Building [Button] ...");
 
-    fseek(file, 0, SEEK_END);
-    long file_size = ftell(file);
-    fseek(file, 0, SEEK_SET);
+    char button[1024] = {0};
+    if (onclick) {
+        snprintf(button, sizeof(button), "<button onclick=\"%s\">%s</button>\n",
+            onclick, content);
+    } else {
+        snprintf(button, sizeof(button), "<button>%s</button>\n", content);
+    }
+    
+    return append_element(page, button);
+}
 
-    char *buffer = malloc(file_size + 1);
-    if (!buffer) return NULL;
+int add_paragraph(Page *page, const char *content) {
+    if (!page || !content) return -1;
+    log_print(0, "Building [Paragraph] ...");
 
-    fread(buffer, 1, file_size, file);
-    buffer[file_size] = '\0';
-    fclose(file);
+    char paragraph[1024] = {0};
+    snprintf(paragraph, sizeof(paragraph), "<p>%s</p>\n", content);
 
-    return buffer;
+    return append_element(page, paragraph);
+}
+
+int add_image(Page *page, const char *src, const char *alt, uint16_t width, uint16_t height) {
+    if (!page || !src) return -1;
+    log_print(0, "Building [Image] ...");
+
+    if (width > 1500 || height > 1500)
+        log_print(-1, "[Warning] Image dimensions are very big!");
+    if (width == 0 || height == 0)
+        log_print(-1, "[Warning] Image dimensions are zero! (Image is not visible)");
+
+    char image[1024] = {0};
+    if (alt) {
+        snprintf(image, sizeof(image), "<img src=\"%s\" width=\"%u\" height=\"%u\" alt=\"%s\">\n", 
+            src, width, height, alt);
+    } else {
+        snprintf(image, sizeof(image), "<img src=\"%s\" width=\"%u\" height=\"%u\">\n", 
+            src, width, height);
+    }
+
+    return append_element(page, image);
 }
